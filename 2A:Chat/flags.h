17a1c38bc1e1372bfc730346f1845003353d6ef4
Caleb Rabbon <crabbon@calpoly.edu>
2020-04-30 15:59:26 -0700

    adding new files

---
+++ 2A:Chat/flags.h
@@ -0,0 +1,17 @@
+#ifndef __FLAGS_H__
+#define __FLAGS_H__
+
+#define FLAG_1 1
+#define FLAG_2 2
+#define FLAG_3 3
+#define FLAG_4 4
+#define FLAG_5 5
+#define FLAG_6 6  // Not Used
+#define FLAG_7 7
+#define FLAG_8 8
+#define FLAG_9 9
+#define FLAG_10 10
+#define FLAG_11 11
+#define FLAG_12 12
+
+#endif
---
+++ 2A:Chat/macros.h
@@ -0,0 +1,13 @@
+#ifndef __MACROS_H__
+#define __MACROS_H__
+
+#define DEFAULT_PDULEN 3
+#define HEADER_LEN 2
+#define DEBUG_FLAG 1
+#define MAX_HANDLE_SIZE 100
+#define CHAT_HEADER_LEN 3
+#define DEBUG_FLAG 1
+#define HANDLE_BYTE 1
+#define MAX_LOGIN_SIZE 104 //Max size of 100 Character handle + 4 Bytes Header
+
+#endif
